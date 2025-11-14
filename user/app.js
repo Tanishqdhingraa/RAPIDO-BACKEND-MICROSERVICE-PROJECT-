b7be044ff0488ff784a80f7779f955417f523307
Tanishq dhingra <dhingratanishq58@gmail.com>
2025-11-14 16:00:43 +0530

    rapido backend code

---
+++ user/app.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require("mongoose");
+const express = require('express')
+const app = express()
+// const connect = require('./db/db')
+// connect()
+const userRoutes = require('./routes/user.routes')
+const cookieParser = require('cookie-parser')
+// const rabbitMq = require('./service/rabbit')
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/userdb");
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log("DB Error:", err);
+  }
+};
+connectDB();
+
+
+// rabbitMq.connect()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+
+app.use('/', userRoutes)
+
+module.exports = app
---
+++ user/db/db.js
@@ -0,0 +1,11 @@
+// const mongoose = require('mongoose');
+
+//  function connect (){
+//     mongoose.connect(process.env.MONGO_URL).then(()=>{
+//         console.log("MONGOOSE IS CONNECTED ");
+//     }).catch(e=>{
+//         console.log("e");
+//     })
+// }
+
+// module.export = connect;
